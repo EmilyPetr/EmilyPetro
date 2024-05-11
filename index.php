e7a34773fc26db9a4d1267be0011fe239b867fda
Your Name <you@example.com>
2024-05-11 21:01:18 +0200

    prebacivanje na git posle prvog casa

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php include 'template_parts/header.php'; ?>
+
+<?php include 'template_parts/main-header.php'; ?>
+
+<?php include 'template_parts/footer.php'; ?>
---
+++ template_parts/main-header.php
@@ -0,0 +1,10 @@
+ <!--main header-->
+ <header class="mainHeader">
+    <img src="img/coffee-slajd-1.jpg" alt="">
+<hgroup>
+<h1>VREME JE ZA KAFICU</h1>
+<p>Prva pauza je rezervisana za pauzu</p>
+<a href="" class="btn btn-yellow">Saznaj vise</a>
+</hgroup>
+
+</header>
