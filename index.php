c2a5cc0f49014f251e8eb97df75e54068e08222f
Your Name <146434041+EmilyPetr@users.noreply.github.com>
2024-05-15 18:03:46 +0200

    ponuda part

--- index.php
+++ index.php
@@ -2,4 +2,6 @@
 
 <?php include 'template_parts/main-header.php'; ?>
 
+<?php include 'template_parts/part-ponuda.php'; ?>
+
 <?php include 'template_parts/footer.php'; ?>
---
+++ template_parts/part-ponuda.php
@@ -0,0 +1,45 @@
+<!--ponuda-->
+<section class="ponuda container py text-center">
+    <h2>ŠTA VAM NUDIMO</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, est?
+
+    </p>
+    <article>
+        <div>
+            <img src="img/coffee1.jpg" alt="">
+            <div>
+            <h3>DORUČAK DO 11
+            </h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore consequuntur suscipit impedit nobis doloribus et fugit voluptates fugiat error sint!
+
+            </p>
+            </div>
+        </div>
+
+        <div>
+            <img src="img/coffee3.jpg" alt="">
+            <div>
+            <h3>DORUČAK DO 11
+            </h3>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi impedit id ipsa deserunt culpa voluptatibus recusandae possimus cupiditate unde vitae.
+
+            </p>
+            </div>
+        </div>
+
+        <div>
+            <img src="img/coffee2.jpg" alt="">
+            <div>
+            <h3>DORUČAK DO 11
+            </h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia a aliquid ducimus cumque, qui est eius. Atque ipsam odio doloribus!
+
+
+            </p>
+            </div>
+        </div>
+    </article>
+</section>
+
+
+
