bb4db78108bc8e23b5df3bebbcb21ad971e5fbb9
Your Name <146434041+EmilyPetr@users.noreply.github.com>
2024-05-20 13:39:37 +0200

    part usluge

---
+++ template_parts/part-usluge.php
@@ -0,0 +1,49 @@
+<!--usluge-->
+<section class="usluge container py">
+    <h2 class="text-center">
+    ZABAVITE SE KOD NAS
+    </h2>
+    <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi quam saepe voluptas atque, facere odio!
+
+    </p>
+
+    <article>
+        <div>
+            <img src="img/coffee 12.jpg" alt="">
+        </div>
+        <div>
+            <h3>TEAM BUILDING</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laborum vitae delectus distinctio asperiores! Quisquam sunt cum aliquam ut quo?
+
+            </p>
+        </div>
+        
+    </article>
+
+    <article>
+        <div>
+            <img src="img/coffee 13.jpg" alt="">
+        </div>
+        <div>
+            <h3>PROMOCIJE</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laborum vitae delectus distinctio asperiores! Quisquam sunt cum aliquam ut quo?
+                
+            </p>
+        </div>
+        
+    </article>
+
+    <article>
+        <div>
+            <img src="img/coffee 14.jpg" alt="">
+        </div>
+        <div>
+            <h3>RADIONICE</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti laborum vitae delectus distinctio asperiores! Quisquam sunt cum aliquam ut quo?
+                
+            </p>
+        </div>
+        
+    </article>
+</section>
+
